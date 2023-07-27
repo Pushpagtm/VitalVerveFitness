ff13d9e38b306012a1637d5fbc9bb9a912474351
Pushpagtm <pushpagautam529@gmail.com>
2023-07-27 22:05:09 +0545

    created react app with typescript and tailwind

---
+++ vitalverve/src/App.tsx
@@ -0,0 +1,15 @@
+import Navbar from '@/scenes/navbar';
+import './App.css'
+
+function App() {
+   return (
+    <>
+    <div className='app bg-gray-20'>
+      <Navbar/>
+    </div>
+     
+    </>
+  )
+}
+
+export default App
---
+++ vitalverve/src/scenes/navbar/index.tsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/solid";
+import Logo from "@/assets/Logo.png";
+import Link from "./Link";
+
+type Props = {}
+
+const Navbar = (props: Props) => {
+  return (
+    <div>index</div>
+  )
+}
+
+export default Navbar;
