571c6c895e246f74fd965408f245e0440147da1d
Pushpagtm <pushpagautam529@gmail.com>
2023-07-28 10:49:54 +0545

    navbar completed

--- vitalverve/src/App.tsx
+++ vitalverve/src/App.tsx
@@ -1,13 +1,28 @@
 import Navbar from '@/scenes/navbar';
 import './App.css'
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import { SelectedPage } from './shared/types';
 
 function App() {
-  const [selectedPage, setSelectedPage] = useState('Home');
+  const [selectedPage, setSelectedPage] = useState<SelectedPage>(SelectedPage.Home );
+  const[isTopOfPage,setIsTopOfPage]=useState<boolean>(true)
+  useEffect(()=>{
+    const handleScroll=()=>{
+      if(window.scrollY===0){
+        setIsTopOfPage(true);
+        setSelectedPage(SelectedPage.Home);
+      }else if(window.scrollY !==0){
+        setIsTopOfPage(false);
+      }
+      window.addEventListener("scroll",handleScroll);
+      return ()=>window.removeEventListener("scroll",handleScroll)
+    }
+  },[]);
+
    return (
     <>
     <div className='app bg-gray-20'>
-      <Navbar  selectedPage={selectedPage} setSelectedPage={setSelectedPage}/>
+      <Navbar isTopOfPage={isTopOfPage} selectedPage={selectedPage} setSelectedPage={setSelectedPage}/>
     </div>
      
     </>
