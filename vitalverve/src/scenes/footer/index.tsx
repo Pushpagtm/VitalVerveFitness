7f1db0a3c99ad20d224261ec84957e6374a8e298
Pushpagtm <pushpagautam529@gmail.com>
2023-08-02 08:25:19 +0545

    fix bug

--- vitalverve/src/scenes/footer/index.tsx
+++ vitalverve/src/scenes/footer/index.tsx
@@ -8,7 +8,7 @@ const Footer = () => {
         <div className="mt-16 basis-1/2 md:mt-0">
           <img alt="logo" src={Logo} />
           <p className="my-5">
-            Lorem vitae ut augue auctor faucibus eget eget ut libero. Elementum
+          Lorem vitae ut augue auctor faucibus eget eget ut libero. Elementum
             purus et arcu massa dictum condimentum. Augue scelerisque iaculis
             orci ut habitant laoreet. Iaculis tristique.
           </p>
